28bfaada3d9546e1477c431bd05dcd0ab27339e7
KennethCodepuz <kennethseancorpuz14@gmail.com>
2026-07-15 12:47:00 +0800

    add: Call to Action section finish

--- app/components/sections/CallToAction.tsx
+++ app/components/sections/CallToAction.tsx
@@ -6,12 +6,10 @@ type Props = {}
 const CallToAction = (props: Props) => {
    return (
       <section id='call-to-action' className='px-6 py-14 flex flex-col items-center justify-center bg-[var(--background-dark-100)]'>
-         <div className='flex flex-col items-center justify-center gap-5'>
-            <h1 className='text-3xl leading-[1.4]'>Let's Connect</h1>
-            <p className='text-lg text-center leading-normal text-[var(--text-white-40)]'>I'm currently open to new opportunities, whether they're full-time roles, freelance projects, or collaborations.</p>
-            <div className='flex gap-4'>
-               <ButtonRightArrow className='px-4 py-3 text-sm'>Contact Me</ButtonRightArrow>
-            </div>
+         <div className='py-[6.25rem] px-6 flex flex-col items-center justify-center gap-5 bg-[linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(/CreativeShot.jpeg)] bg-center bg-cover rounded-[1.25rem]'>
+            <h1 className='text-3xl leading-[1.4] tracking-[-0.02em]'>Let's Connect</h1>
+            <p className='text-lg text-center leading-normal text-[var(--text-white-40)]'>I'm currently open to new opportunities, whether they're full-time roles, freelance projects, or Internships.</p>
+            <ButtonRightArrow className='px-4 py-3 text-sm'>Contact Me</ButtonRightArrow>
          </div>
       </section>
    )
